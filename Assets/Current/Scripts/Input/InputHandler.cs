cca8d94ce5138f7d830a9d1bfad55f53a197f371
SeanTRyan <ryans@my.lanecc.edu>
2018-02-28 10:17:58 -0800

    Different structure of code. I think it is cleaner and more mutable. Also, trying out a input system.

---
+++ Assets/Current/Scripts/Input/InputHandler.cs
@@ -0,0 +1,44 @@
+﻿using InControl;
+using Player.Locomotion;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public enum PlayerNumber { Player1, Player2, Player3, Player4, Count }
+    public PlayerNumber playerNum;
+
+    Movement movement;
+    IUpdate update;
+    InputDevice inputDevice;
+
+    private void Start()
+    {
+        movement = GetComponent<Movement>();
+
+        string name = Input.GetJoystickNames()[(int)playerNum];
+
+        update = GetComponent<IUpdate>();
+
+        print(update);
+
+        inputDevice = (InputManager.Devices.Count > (int)playerNum) ? InputManager.Devices[(int)playerNum] : null;
+        if (inputDevice == null)
+            throw new System.Exception("ERROR");
+    }
+
+    private void Update()
+    {
+        movement.OnUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        update.OnFixedUpdate(inputDevice);
+        //movement.OnFixedUpdate(inputDevice);
+    }
+
+    public void OnGUI()
+    {
+        GUI.Label(new Rect(100f, 100f, 100f, 100f), "Input device ");
+    }
+}
---
+++ Assets/Deprecated/_Scripts/Player/Attacks/Attack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Attack : ScriptableObject
+{
+    [HideInInspector] public string Name = "New Attack";
+    [HideInInspector] public AudioClip Sound;
+    [HideInInspector] public float CoolDown = 1.0f;
+
+    public abstract void Initialize(GameObject obj);
+    public abstract void TriggerAbility();
+}
